fd94b4ee8ea2dd77404f68f63cf969a570ed0d96
José Victoriano <jose.victoriano@wakaru.eu>
2018-12-04 18:17:24 +0000

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,47 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ResponsiveWebDevelopment';
+
+
+
+
+  expandDiv1(){
+    var d1 = document.getElementById("div1");
+    var d2 = document.getElementById("div2");
+    var d3 = document.getElementById("div3");
+    // d1.style.width = "90%";
+    // d2.style.width = "5%";
+    // d3.style.width = "5%";
+    d3.style.display = "none";
+    d2.style.display = "none";
+  }
+
+  expandDiv2(){
+    var d1 = document.getElementById("div1");
+    var d2 = document.getElementById("div2");
+    var d3 = document.getElementById("div3");
+    var bl =document.getElementById("openLeft");
+    var br =document.getElementById("openRight");
+    // d1.style.width = "5%";
+    // d2.style.width = "90%";
+    // d3.style.width = "5%";
+    d3.style.display = "none";
+    d1.style.display = "none";
+    bl.style.display = "block";
+    br.style.display = "block";
+  }
+
+
+  expandDiv3(){
+    var d1 = document.getElementById("div1");
+    var d2 = document.getElementById("div2");
+    var d3 = document.getElementById("div3");
+    // d1.style.width = "5%";
+    // d2.style.width = "5%";
+    // d3.style.width = "90%";
+    d1.style.display = "none";
+    d2.style.display = "none";
+  }
+
+
 }
